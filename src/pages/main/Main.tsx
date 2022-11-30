6dfca3e569e7e0b03897dbbb48f763fb740cb242
Jose Toro <ing.josetoro.2@gmail.com>
2022-11-29 21:11:27 -0500

    test non import image module

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,18 @@
 import './Main.scss';
-import images from './Main.Images';
+// import images from './Main.Images';
+
+const images = {
+	hero: {
+		avif: 'public/main_hero.avif',
+		webp: 'public/main_hero.webp',
+		jpg: 'public/main_hero.jpg',
+	},
+	sideImage: {
+		avif: 'public/main_side-image.avif',
+		webp: 'public/main_side-image.webp',
+		jpg: 'public/main_side-image.jpg',
+	},
+};
 
 function Main() {
 	return (
