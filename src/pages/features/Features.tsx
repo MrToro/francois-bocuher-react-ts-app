5a1efc869e355f9602780a6e9850e2b07b0399ab
Jose Toro <ing.josetoro.2@gmail.com>
2022-11-30 09:43:46 -0500

    Features styles

--- src/pages/features/Features.tsx
+++ src/pages/features/Features.tsx
@@ -1,27 +1,30 @@
+import './Features.scss';
 import images from './Features.Images';
 
 function Features() {
   return (
-		<>
-			<h2>What colors! what a variety! what a wealth of objects and ideas! This man has everything except the truth</h2>
+		<section className='container features'>
+			<h2 className='features__title'>What colors! what a variety! what a wealth of objects and ideas! This man has everything except the truth</h2>
 
-			<picture>
-				<source srcSet={images.features_1.avif} type='image/avif' />
-				<source srcSet={images.features_1.webp} type='image/webp' />
-				<img src={images.features_1.def} alt='...' loading='lazy' decoding='async' draggable='false' />
-			</picture>
+			<div className='features__images-container'>
+				<picture className='features__main-image'>
+					<source srcSet={images.features_1.avif} type='image/avif' />
+					<source srcSet={images.features_1.webp} type='image/webp' />
+					<img src={images.features_1.def} alt='...' loading='lazy' decoding='async' draggable='false' />
+				</picture>
 
-			<picture>
-				<source srcSet={images.features_2.avif} type='image/avif' />
-				<source srcSet={images.features_2.webp} type='image/webp' />
-				<img src={images.features_2.def} alt='...' loading='lazy' decoding='async' draggable='false' />
-			</picture>
+				<picture className='features__side-image'>
+					<source srcSet={images.features_2.avif} type='image/avif' />
+					<source srcSet={images.features_2.webp} type='image/webp' />
+					<img src={images.features_2.def} alt='...' loading='lazy' decoding='async' draggable='false' />
+				</picture>
 
-			<p>
-				Artists who see how far this man has overcome the difficulties of painting and for whom it is all this merit that is hardly well known to them bend their knees before him.
-				He is their god. The others don't care
-			</p>
-		</>
+				<p className='features__description-container'>
+					Artists who see how far this man has overcome the difficulties of painting and for whom it is all this merit that is hardly well known to them bend their knees before
+					him. He is their god. The others don't care
+				</p>
+			</div>
+		</section>
 	);
 }
 
