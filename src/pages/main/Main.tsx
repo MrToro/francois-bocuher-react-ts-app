635b1a8dfd7df187d0fddd873d7b9623391bba98
Jose Toro <ing.josetoro.2@gmail.com>
2022-11-29 21:20:14 -0500

    Update package.json

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,18 +1,5 @@
 import './Main.scss';
-// import images from './Main.Images';
-
-const images = {
-	hero: {
-		avif: 'public/main_hero.avif',
-		webp: 'public/main_hero.webp',
-		jpg: 'public/main_hero.jpg',
-	},
-	sideImage: {
-		avif: 'public/main_side-image.avif',
-		webp: 'public/main_side-image.webp',
-		jpg: 'public/main_side-image.jpg',
-	},
-};
+import images from './Main.Images';
 
 function Main() {
 	return (
